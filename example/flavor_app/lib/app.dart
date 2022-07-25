13f35cc771e9d83023d2ef3a4dbf1a9fdee10cba
Long Kimhak <longkimhak.kh@gmail.com>
2022-07-25 09:44:51 +0700

    format

--- example/flavor_app/lib/app.dart
+++ example/flavor_app/lib/app.dart
@@ -25,17 +25,17 @@ class App extends StatelessWidget {
   }) =>
       show
           ? Banner(
-        child: child,
-        location: BannerLocation.topStart,
-        message: F.name,
-        color: Colors.green.withOpacity(0.6),
-        textStyle: TextStyle(
-            fontWeight: FontWeight.w700,
-            fontSize: 12.0,
-            letterSpacing: 1.0),
-        textDirection: TextDirection.ltr,
-      )
+              child: child,
+              location: BannerLocation.topStart,
+              message: F.name,
+              color: Colors.green.withOpacity(0.6),
+              textStyle: TextStyle(
+                  fontWeight: FontWeight.w700,
+                  fontSize: 12.0,
+                  letterSpacing: 1.0),
+              textDirection: TextDirection.ltr,
+            )
           : Container(
-        child: child,
-      );
+              child: child,
+            );
 }

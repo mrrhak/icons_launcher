588328fde5b940f1619ad6e9c06a1c3ed6060251
Mrr Hak <longkimhak.kh@gmail.com>
2026-03-19 00:12:13 +0700

    chore(android): add obfuscation should be enabled for release builds

--- example/simple_app/android/app/build.gradle.kts
+++ example/simple_app/android/app/build.gradle.kts
@@ -35,6 +35,7 @@ android {
             // TODO: Add your own signing config for the release build.
             // Signing with the debug keys for now, so `flutter run --release` works.
             signingConfig = signingConfigs.getByName("debug")
+            isMinifyEnabled = true
         }
     }
 }
